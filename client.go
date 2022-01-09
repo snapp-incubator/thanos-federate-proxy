ec3e2a3de19dbad05c4480c1ff034797d2499993
warpcomdev <soporte@warpcom.com>
2022-01-09 19:07:11 +0100

    foce GET

--- client.go
+++ client.go
@@ -4,10 +4,9 @@ import (
 	"bufio"
 	"context"
 	"fmt"
-	"io"
 	"io/fs"
+	"io/ioutil"
 	"net/http"
-	"net/url"
 	"strings"
 
 	"github.com/prometheus/client_golang/api"
@@ -39,24 +38,21 @@ func (c client) Do(ctx context.Context, req *http.Request) (*http.Response, []by
 	if req.Header == nil {
 		req.Header = make(http.Header)
 	}
-	params := req.URL.Query()
-	if req.Method == http.MethodPost && req.Header.Get("Content-Type") == "application/x-www-form-urlencoded" {
-		payload, err := io.ReadAll(req.Body)
-		req.Body.Close()
-		if err != nil {
-			return nil, nil, err
-		}
-		postParams, err := url.ParseQuery(string(payload))
-		if err != nil {
-			return nil, nil, err
-		}
-		for name, values := range postParams {
-			for _, value := range values {
-				params.Add(name, value)
-			}
-		}
-		req.Method = http.MethodGet
+	// Force use GET
+	if req.Method == http.MethodPost {
+		return &http.Response{
+			Status:        "Method Not Allowed",
+			StatusCode:    http.StatusMethodNotAllowed,
+			Proto:         req.Proto,
+			ProtoMajor:    req.ProtoMajor,
+			ProtoMinor:    req.ProtoMinor,
+			Body:          ioutil.NopCloser(nil),
+			ContentLength: 0,
+			Request:       req,
+			Header:        make(http.Header, 0),
+		}, nil, nil
 	}
+	params := req.URL.Query()
 	params.Add("namespace", "smc-os2-tools")
 	req.URL.RawQuery = params.Encode()
 	req.Header.Set("Authorization", c.authz)
