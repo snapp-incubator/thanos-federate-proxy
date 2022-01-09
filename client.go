785af386dbe7acdad24039cd20b402c2e348ef9d
warpcomdev <soporte@warpcom.com>
2022-01-09 15:48:34 +0100

    Added support for bearer-file option
    Debug (#1)
    
    * Added debug text
    
    * test
    
    * test add namespace
    
    * changed namespace
    
    * test GET instead of POST
    
    * foce GET
    
    * Improved support for openshift
    
    * tidy modules
    
    * fixed typo
    
    Co-authored-by: warpcomdev <soporte@warpcom.com>

---
+++ client.go
@@ -0,0 +1,154 @@
+package main
+
+import (
+	"bufio"
+	"context"
+	"fmt"
+	"io/fs"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"strings"
+
+	"github.com/prometheus/client_golang/api"
+)
+
+// ClientConfigError returned when the token file is empty or invalid
+type ClientConfigError string
+
+// Error implements error
+func (err ClientConfigError) Error() string {
+	return string(err)
+}
+
+const (
+	EmptyBearerFileError    = ClientConfigError("First line of bearer token file is empty")
+	InvalidBearerTokenError = ClientConfigError("Bearer token must be ASCII")
+	NilOptionError          = ClientConfigError("configOption cannot be nil")
+)
+
+// Client wraps prometheus api.Client to add custom headers to every request
+type client struct {
+	api.Client
+	authz string // Authorization header
+	asGet bool   // True to reject POST requests
+}
+
+type paramKey int
+
+// addValues inserts the provided request params in context
+func addValues(ctx context.Context, params url.Values) context.Context {
+	return context.WithValue(ctx, paramKey(0), params)
+}
+
+// getValues extracts from context the params provided by addParams
+func getValues(ctx context.Context) (url.Values, bool) {
+	if ctxValue := ctx.Value(paramKey(0)); ctxValue != nil {
+		if params, ok := ctxValue.(url.Values); ok {
+			return params, true
+		}
+	}
+	return nil, false
+}
+
+// Do implements api.Client
+func (c client) Do(ctx context.Context, req *http.Request) (*http.Response, []byte, error) {
+	if c.asGet && req.Method == http.MethodPost {
+		// If response to POST is http.StatusMethodNotAllowed,
+		// Prometheus api library will failover to GET.
+		return &http.Response{
+			Status:        "Method Not Allowed",
+			StatusCode:    http.StatusMethodNotAllowed,
+			Proto:         req.Proto,
+			ProtoMajor:    req.ProtoMajor,
+			ProtoMinor:    req.ProtoMinor,
+			Body:          ioutil.NopCloser(nil),
+			ContentLength: 0,
+			Request:       req,
+			Header:        make(http.Header, 0),
+		}, nil, nil
+	}
+	// If context includes URL parameters, append them to the query
+	if params, ok := getValues(ctx); ok {
+		reqParams := req.URL.Query()
+		for name, values := range params {
+			for _, value := range values {
+				reqParams.Add(name, value)
+			}
+		}
+		req.URL.RawQuery = reqParams.Encode()
+	}
+	if c.authz != "" {
+		if req.Header == nil {
+			req.Header = make(http.Header)
+		}
+		req.Header.Set("Authorization", c.authz)
+	}
+	return c.Client.Do(ctx, req)
+}
+
+// readBearerToken from given FS and fileName.
+// Takes sys.FS instead of path for easier testing.
+func readBearerToken(fsys fs.FS, fileName string) (string, error) {
+	bearerFile, err := fsys.Open(fileName)
+	if err != nil {
+		return "", err
+	}
+	defer bearerFile.Close()
+	scanner := bufio.NewScanner(bearerFile)
+	if !scanner.Scan() {
+		if err := scanner.Err(); err != nil {
+			return "", err
+		}
+		return "", EmptyBearerFileError
+	}
+	return scanner.Text(), nil
+}
+
+// IsAscii checks if string consists only of ascii characters
+func isAscii(str string) bool {
+	for _, b := range str {
+		if b <= 0 || b > 127 {
+			return false
+		}
+	}
+	return true
+}
+
+// clientOption implements functional options pattern for client
+type clientOption func(c *client) error
+
+// newClient wraps an api.Client adding the given options
+func newClient(c api.Client, opts ...clientOption) (client, error) {
+	result := client{Client: c}
+	if len(opts) > 0 {
+		for _, opt := range opts {
+			// catch wrong calls to newClient(c, nil)
+			if opt == nil {
+				return client{}, NilOptionError
+			}
+			if err := opt(&result); err != nil {
+				return client{}, err
+			}
+		}
+	}
+	return result, nil
+}
+
+// withToken adds Authz bearer token to all requests
+func withToken(bearer string) clientOption {
+	return func(c *client) error {
+		bearer = strings.TrimSpace(bearer)
+		if bearer == "" || !isAscii(bearer) {
+			return InvalidBearerTokenError
+		}
+		c.authz = fmt.Sprintf("Bearer %s", bearer)
+		return nil
+	}
+}
+
+// withGet only allows GET queries
+func withGet(c *client) error {
+	c.asGet = true
+	return nil
+}
---
+++ client_test.go
@@ -0,0 +1,168 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"reflect"
+	"testing"
+	"testing/fstest"
+
+	"github.com/prometheus/client_golang/api"
+)
+
+const (
+	validBearerToken  = "Any Regular ASCII string"
+	validBearerHeader = "Bearer " + validBearerToken
+)
+
+// mockClient does a poor job at mocking prometheus api.Client
+type mockClient struct {
+	header http.Header
+	reqURL *url.URL
+}
+
+// URL implements api.Client
+func (m *mockClient) URL(ep string, args map[string]string) *url.URL {
+	return nil
+}
+
+// Do implements api.Client
+func (m *mockClient) Do(ctx context.Context, req *http.Request) (*http.Response, []byte, error) {
+	m.header = req.Header
+	m.reqURL = req.URL
+	return nil, nil, nil
+}
+
+// TestBearerToken validates bearer token reading and insertion
+func TestBearerToken(t *testing.T) {
+
+	fsys := fstest.MapFS{
+		"empty_bearer": {
+			Data: []byte{},
+		},
+		"empty_first_line_bearer": {
+			Data: []byte("\nFirst line cannot be emtpy"),
+		},
+		"invalid_bearer": {
+			Data: []byte("bearer must be ascii: áéíóú"),
+		},
+		"valid_bearer": {
+			Data: []byte(validBearerToken),
+		},
+		"valid_bearer_with_whitespace": {
+			Data: []byte("   " + validBearerToken + "  "),
+		},
+	}
+
+	read := func(fileName string) (*mockClient, api.Client, error) {
+		m := &mockClient{}
+		b, err := readBearerToken(fsys, fileName)
+		if err != nil {
+			return m, nil, err
+		}
+		c, err := newClient(m, withToken(b))
+		return m, c, err
+	}
+
+	// Check invalid files raise proper errors
+	invalid := map[string]error{
+		"empty_bearer":            EmptyBearerFileError,
+		"empty_first_line_bearer": InvalidBearerTokenError,
+		"invalid_bearer":          InvalidBearerTokenError,
+	}
+	for fileName, expectedErr := range invalid {
+		t.Run("Read bearer from "+fileName, func(t *testing.T) {
+			_, _, err := read(fileName)
+			if !errors.Is(err, expectedErr) {
+				t.Fatal(fmt.Sprintf("Expected error %v, got %v", expectedErr, err))
+			}
+		})
+	}
+
+	// Check valid files send proper token
+	valid := []string{
+		"valid_bearer",
+		"valid_bearer_with_whitespace",
+	}
+	for _, fileName := range valid {
+		t.Run("Read bearer from "+fileName, func(t *testing.T) {
+			m, c, err := read(fileName)
+			if err != nil {
+				t.Fatal(fmt.Sprintf("Expected no error, got %v", err))
+			}
+			req, _ := http.NewRequest(http.MethodGet, "http://localhost/test", nil)
+			c.Do(context.TODO(), req)
+			if m.header == nil || len(m.header) <= 0 {
+				t.Fatal("Empty headers in request")
+			}
+			if authz := m.header.Get("Authorization"); authz != validBearerHeader {
+				t.Fatal(fmt.Sprintf("Expected Authorization header '%s', got '%s'", validBearerHeader, authz))
+			}
+		})
+	}
+}
+
+// TestWithGet validates GET request filtering
+func TestWithGet(t *testing.T) {
+
+	post := func(t *testing.T, opts ...clientOption) *http.Response {
+		c, err := newClient(&mockClient{}, opts...)
+		if err != nil {
+			t.Fatalf("Expected no error, got %v", err)
+		}
+		req, _ := http.NewRequest(http.MethodPost, "http://localhost/test", io.NopCloser(bytes.NewReader([]byte{})))
+		req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+		res, _, err := c.Do(context.TODO(), req)
+		if err != nil {
+			t.Fatalf("Expected no error, got %v", err)
+		}
+		return res
+	}
+
+	t.Run("regular client permits POST", func(t *testing.T) {
+		if res := post(t); res != nil {
+			t.Fatalf("Expected nil response (from mock), got %v", res)
+		}
+	})
+
+	t.Run("withGet client rejects POST", func(t *testing.T) {
+		res := post(t, withGet)
+		if res == nil {
+			t.Fatal("Expected not-nil response")
+		}
+		if res.StatusCode != http.StatusMethodNotAllowed {
+			t.Fatalf("Expected status code %d, got %v", http.StatusMethodNotAllowed, res)
+		}
+	})
+}
+
+// TestWithValues validates params passing through context
+func TestWithValues(t *testing.T) {
+	m := mockClient{}
+	c, err := newClient(&m)
+	if err != nil {
+		t.Fatalf("Expected no error, got %v", err)
+	}
+	v, _ := url.ParseQuery("key1=val1&key2=val2")
+	ctx := addValues(context.TODO(), v)
+	req, _ := http.NewRequest(http.MethodGet, "http://localhost/test", nil)
+	if _, _, err := c.Do(ctx, req); err != nil {
+		t.Fatalf("Expected no error, got %v", err)
+	}
+	urlQuery := m.reqURL.Query()
+	if !reflect.DeepEqual(v, urlQuery) {
+		t.Fatalf("Expected query parameters %v, got %v", v, urlQuery)
+	}
+}
+
+// TestNilOption makes sure config doesn't panic if a nil option is passed
+func TestNilOption(t *testing.T) {
+	if _, err := newClient(&mockClient{}, nil); !errors.Is(err, NilOptionError) {
+		t.Fatalf("Expected error %v, got %v", NilOptionError, err)
+	}
+}
