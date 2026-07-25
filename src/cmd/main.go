1fb91e35c93c608430dc608bf23ed3e837eed78e
navid.shariaty <navid.shariaty@snapp.cab>
2026-07-25 13:48:44 +0330

    update ci

--- src/cmd/main.go
+++ src/cmd/main.go
@@ -91,7 +91,7 @@ func Federate(_ context.Context, w http.ResponseWriter, r *http.Request, apiClie
 		if restriction.MetricName == "" {
 			paramBuilder = fmt.Sprintf("{%s}", labelBuilder)
 		} else if restriction.MetricLabels == nil {
-			paramBuilder = fmt.Sprintf("%s", restriction.MetricName)
+			paramBuilder = restriction.MetricName
 		} else {
 			paramBuilder = fmt.Sprintf("%s{%s}", restriction.MetricName, labelBuilder)
 		}
